2011afaf4366ff787bd99401a7c8e11e75eb6cba
Nora Söderlund <nora.soderlund@polestar.com>
2023-09-02 14:25:05 +0200

    Fix missing exports

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,26 @@ export * from "./constants";
 
 export * from "./requests/getDataLayers";
 export * from "./requests/getTiff";
+export * from "./requests/findClosestBuildingInsights"
 
+export * from "./types/BuildingInsights";
+export * from "./types/CashPurchaseSavings";
 export * from "./types/DataLayerView";
 export * from "./types/DataLayers";
 export * from "./types/Date";
+export * from "./types/FinancedPurchaseSavings";
+export * from "./types/FinancialAnalysis";
+export * from "./types/FinancialDetails";
 export * from "./types/ImageryQuality";
+export * from "./types/LatLng";
+export * from "./types/LatLngBox";
+export * from "./types/LeasingSavings";
+export * from "./types/Money";
+export * from "./types/RoofSegmentSizeAndSunshineStats";
+export * from "./types/RoofSegmentSummary";
+export * from "./types/SavingsOverTime";
+export * from "./types/SizeAndSunshineStats";
+export * from "./types/SolarPanel";
+export * from "./types/SolarPanelConfig";
+export * from "./types/SolarPanelOrientation";
+export * from "./types/SolarPotentional";
